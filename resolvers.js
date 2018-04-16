45c48d45576b59b84e5d06af0fced2d4bee4ce5f
iurii-kyrylenko <yuriproject@gmail.com>
2018-04-16 16:06:28 +0300

    proceeded with UserConnection

--- resolvers.js
+++ resolvers.js
@@ -20,9 +20,68 @@ const users = (root, args, ctx) => {
     return aggregator;
 };
 
+const getPageUsers = (root, { search, first, after }, ctx) => {
+  const User = ctx.mongoose.model('User');
+
+  const aggregator = User.aggregate()
+    .lookup({ from: "books", localField: "_id", foreignField: "userId", as: "books" })
+    .lookup({ from: "movies", localField: "_id", foreignField: "userId", as: "movies" })
+    .project({
+      _id: 0,
+      id: '$_id',
+      name: 1,
+      email: 1,
+      shareBooks: 1,
+      shareMovies: 1,
+      books: { $size: "$books" },
+      movies: { $size: "$movies" },
+      total: { $add: [{ $size: '$books' }, { $size: '$movies' }] },
+    })
+    .sort({ total: -1, name: 1 });
+
+  const sortObject = cursorToSortObject(after);
+
+  if(sortObject) {
+    const { name, total } = sortObject;
+    aggregator.match({
+      $or: [
+        { total: { $lt: total } },
+        { $and: [{ total: { $eq: total } }, { name: { $gt: name } }] }
+      ]
+    });
+  }
+
+  return aggregator;
+};
+
+const sortObjectToCursor = ({ name, total }) => {
+  const stringFromObject = JSON.stringify({ name, total });
+  return Buffer.from(stringFromObject).toString('base64');
+};
+
+const cursorToSortObject = cursor => {
+  if (!cursor || cursor === '') return null;
+  const stringFromBase64 = Buffer.from(cursor, 'base64').toString();
+  return JSON.parse(stringFromBase64);
+};
+
+const pgUsers = async (root, args, ctx) => {
+  const users = await getPageUsers(root, args, ctx);
+  return {
+    edges: users.map(user => ({
+      node: user,
+      cursor: sortObjectToCursor(user)
+    })),
+    pageInfo: {
+      hasNextPage: false
+    }
+  };
+};
+
 const resolvers = {
   Query: {
     users,
+    pgUsers,
     books: () => [],
     movies: () => [],
   }
--- typeDefs.js
+++ typeDefs.js
@@ -34,10 +34,26 @@ const typeDefs = `
     shareMovies: Boolean
     books: Int
     movies: Int
+    total: Int
+  }
+
+  type UserConnection {
+    edges: [UserEdge]
+    pageInfo: PageInfo!
+  }
+
+  type UserEdge {
+    cursor: String!
+    node: User!
+  }
+
+  type PageInfo {
+    hasNextPage: Boolean!
   }
 
   type Query {
     users: [User]!
+    pgUsers(search: String = "", first: Int = 5, after: String = ""): UserConnection
     books(userId: ID!): [Book]!
     movies(userId: ID!): [Movie]!
   }
