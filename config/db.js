bff06ba79be8b6ff75a1524a7d8ef046750a514a
ikyrylenko <ikyrylenko@brightgrove.com>
2018-12-05 17:51:53 +0200

    Serialize mongo IDs to work with GraphQL

--- config/db.js
+++ config/db.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
+const ObjectId = require('mongoose').Types.ObjectId;
+
 mongoose.Promise = global.Promise;
+
+// GraphQL uses a special method valueOf to serialize ID in object type
+// https://github.com/apollographql/apollo-server/issues/1633
+//
+ObjectId.prototype.valueOf = function () {
+	return this.toString();
+};
 
 mongoose.connect(process.env.CONNECTION_STRING);
 
